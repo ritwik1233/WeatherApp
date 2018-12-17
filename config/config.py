777fab35913921711fdde15decfc5a6cb86058bb
ritwik1233 <ritwiksinha25@gmail.com>
2018-12-17 17:17:39 +0530

    First Commit

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 
 
 
-weatherappID='d057499719f262e76f0c912314e06e6e'
-geocodeApiID='AIzaSyAU_ErOREcU8JI5H9xETABf0P8QzqjXMHw'
-weatherURL='https://api.darksky.net/forecast/'
-mongodbURL='mongodb://weatherApp:password12@ds129484.mlab.com:29484/weather_app'
-geoCodeURL='https://maps.googleapis.com/maps/api/geocode/json'
+weatherappID='newWeatherAppID'
+geocodeApiID='geocodeAPiID'
+weatherURL='weatherAppURL'
+mongodbURL='DB_URL'
+geoCodeURL='geoCOdeURL'
